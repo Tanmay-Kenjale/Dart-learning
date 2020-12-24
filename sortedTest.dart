f915993827dcff1420601bba25b1658ffca9261a
Tanmay <tkenjale110@gmail.com>
2020-12-24 17:52:52 +0530

    plan ready now only bubble sort

---
+++ sortedTest.dart
@@ -0,0 +1,23 @@
+int index(dynamic list, dynamic subject) {
+  for (dynamic i = 0; i <= list.length;) {
+    if (list[i] == subject) {
+      return i;
+    }
+    i = i + 1;
+  }
+}
+
+bool sorted(dynamic list) {
+  bool result = false;
+  dynamic lastNum = 0;
+  for (dynamic char in list) {
+    result = false;
+    if (char > lastNum) {
+      result = true;
+    }
+    lastNum = char;
+  }
+  return result;
+}
+
+
