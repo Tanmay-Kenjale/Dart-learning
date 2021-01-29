e9f440bc004ac32a2d1a69da7a5585713bc52fcf
Tanmay <tkenjale110@gmail.com>
2021-01-29 17:03:18 +0530

    added midCharChars

---
+++ logicalProblems/midCharChars.dart
@@ -0,0 +1,23 @@
+String midCharChars(String str) {
+  /* Returns a single character if the length of string is odd. 
+  If it is even returns 2 characters */
+
+  String result = "";
+
+  if (str.length % 2 != 0) {
+    int length = str.length;
+    String mid = str[length ~/ 2];
+    result = result + mid;
+  } else {
+    int length = str.length;
+    String secChar = str[length ~/ 2];
+    String firsChar = str[(length ~/ 2) - 1];
+    result = result + firsChar + secChar;
+  }
+
+  return result;
+}
+
+void main() {
+  print(midCharChars("crates"));
+}
