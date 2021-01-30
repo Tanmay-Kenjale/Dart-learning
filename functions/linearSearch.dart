b684234ee91c7bd00d3ea474eb2f79b512b065de
Tanmay <tkenjale110@gmail.com>
2021-01-30 15:05:55 +0530

    mere changes to sort and linearSearch

---
+++ functions/linearSearch.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+int linearSearch(List subj, dynamic element) {
+  /* Returns the index of the element from the list */
+
+  int count = 0;
+
+  if (subj.contains(element)) {
+    for (dynamic i in subj) {
+      count = count + 1;
+      if (i == element) {
+        break;
+      }
+    }
+  } else {
+    exit(1);
+  }
+
+  return count - 1;
+}
+
+void main() {
+  print(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9));
+}
