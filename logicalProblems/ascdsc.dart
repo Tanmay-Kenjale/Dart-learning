13c23c90a6164d646417fbb77440a5c73d903e8c
Tanmay <tkenjale110@gmail.com>
2021-02-03 14:36:01 +0530

    changes in settings

--- logicalProblems/ascdsc.dart
+++ logicalProblems/ascdsc.dart
@@ -1,6 +1,6 @@
 ascdesc(int dig1, int dig2, int dig3) {
   /* will check if the three digits are in ascending or descending order */
-  
+
   if (dig1 <= dig2 && dig2 <= dig3) {
     print("in order");
   } else if (dig1 >= dig2 && dig2 >= dig3) {
@@ -9,4 +9,3 @@ ascdesc(int dig1, int dig2, int dig3) {
     print("not in order");
   }
 }
-
