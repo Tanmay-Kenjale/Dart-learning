4203a8acfe5c2bf5454ccd25566f4fbd21bf245f
Tanmay <tkenjale110@gmail.com>
2021-03-04 19:27:04 +0530

    added negPose

---
+++ praticeProblems/negPose.dart
@@ -0,0 +1,11 @@
+void negPos(int x) {
+  /* A program that determines whether an integer is positive, negative or 0. */
+  if (x == 0) {
+    print("X is 0");
+  } else if (x < 0) {
+    print("X is a negative integer");
+  } else {
+    print("X is a positive integer");
+  }
+}
+
